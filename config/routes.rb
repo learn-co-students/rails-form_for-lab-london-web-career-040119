f6feaa472c930fbb4749e350aeb9248250550094
francosta <francisco@fcosta.pt>
2019-05-01 12:00:39 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,6 @@
 Rails.application.routes.draw do
-  get 'school_classes/new'
-
-  get 'school_classes/create'
-
-  get 'school_classes/show'
-
-  get 'school_classes/edit'
-
-  get 'students/new'
-
-  get 'students/create'
-
-  get 'students/show'
-
-  get 'students/edit'
+  resources :students
+  resources: school_classes
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
